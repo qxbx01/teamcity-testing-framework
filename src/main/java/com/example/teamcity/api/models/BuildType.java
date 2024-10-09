71c1aa183e621e83f71a29bd8290848836f82fca
qxbx01 <qxbx01@gmail.com>
2024-10-09 11:44:25 +0200

    Updated BaseTest, BuildType, BuildTypeTest, CheckedBase, Project, TestDataGenerator, User
    Added ProjectTest, TestData, TestDataStorage

--- src/main/java/com/example/teamcity/api/models/BuildType.java
+++ src/main/java/com/example/teamcity/api/models/BuildType.java
@@ -1,5 +1,6 @@
 package com.example.teamcity.api.models;
 
+import com.example.teamcity.api.annotations.Parameterizable;
 import com.example.teamcity.api.annotations.Random;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -14,9 +15,12 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BuildType extends BaseModel {
     @Random
+    @Parameterizable
     private String id;
     @Random
     private String name;
+    @Random
+    @Parameterizable
     private Project project;
     private Steps steps;
 }
